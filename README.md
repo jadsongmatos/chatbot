d1cdd45c64dcc8aea88e6f2953bbec4d7366942c
jadsongmatos <jadson.g-matos@outlook.com>
2023-04-20 02:50:52 -0300

    Refactor app and readme for improved readability and maintainability

--- README.md
+++ README.md
@@ -1,33 +1,30 @@
 # ChatBOT
 
+Trata-se de uma aplicação web Flask que se integra ao Dialogflow do Google Cloud e utiliza o modelo Bloom do projeto BigScience para geração de texto. O aplicativo cria novas intenções no Dialogflow com base nas consultas do usuário e usa o texto gerado como resposta.
+
+Aqui está uma breve explicação dos principais componentes:
+
+1. Importe as bibliotecas necessárias e crie uma instância do aplicativo Flask.
+2. Carregue o modelo Bloom e o tokenizer.
+3. Defina o ID do projeto do Google Cloud e carregue as credenciais.
+4. Crie um cliente de intenções do Dialogflow.
+5. Defina a função `generate(prompt)` que recebe um prompt e gera uma resposta usando o modelo Bloom.
+6. Defina a função `create_intent(pergunta)` que recebe uma pergunta e cria uma nova intenção no Dialogflow com a resposta gerada como o texto da mensagem.
+7. Defina a função `webhook()`, que é chamada quando o aplicativo recebe uma solicitação POST. Essa função extrai a consulta do usuário, cria uma nova intenção usando a função `create_intent()` e retorna uma resposta JSON.
+8. Execute o aplicativo Flask.
+
 install dependencies:
 
 ```bash
-npm install
+pip install -r requirements.txt
 ```
 
 run the app:
 
 ```bash
-npm start
-```
-
-## PYTHON
-
-run the app:
-
-```bash
-python -m py_compile app.py
+python app.py
 ```
 
 ```bash
 uwsgi --ini uwsgi.ini --socket 0.0.0.0:5000 --protocol=http
 ```
-
-### cache
-
-```bash
-python ./__pycache__/app.cpython-310.pyc
-```
-
-
--- app.py
+++ app.py
@@ -3,11 +3,15 @@ import json
 import os
 import hashlib
 
+from concurrent.futures import ThreadPoolExecutor
+
 from google.cloud import dialogflow_v2 as dialogflow
 from google.oauth2 import service_account
 
 from transformers import AutoTokenizer, AutoModelForCausalLM
 # import torch
+
+executor = ThreadPoolExecutor(max_workers=1)
 
 model = AutoModelForCausalLM.from_pretrained(
     "bigscience/bloom-560m", use_cache=True)
@@ -18,13 +22,8 @@ app = Flask(__name__)
 project_id = "bloom-560m-oxcs"  # Substitua pelo ID do seu projeto no GCP
 
 # Carregue suas credenciais
-credentials = service_account.Credentials.from_service_account_info(
-    {
-        "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDIbI73s5cVrbtt\nyVRlEXRa7yEJeDNRDTFOCv00osab3eA0Sr30UaKa/QRFcvIc5SU+sHA72IpeRvYT\nRGQc94JuiJ7yCrk94IIeC64wnwOep3Pj4dTmuE2cbz8DRUhZNnUWYFY6AOJ2h/qE\nFXOlK/DusEFOdnsKdE47p3CHLikKn36ivkpMPDhvoVvjPND3LDutKUJ20+6VtDE6\nR0lGqVUQlcbcxaOYLRD5wh7B5e8Q0yOYSq7Pu3cUVXZpa99In0DLsFyPKRgIsdSa\nqZcH6ck3ZLt78TwsUFuMRCnunxNUerCNP2djEdx2ae6j+ISsco4RNG3XfnoGwwK5\n0qDYZ2x/AgMBAAECggEAALAdnVUMQGV626DXg10d1nJAI0eu7zpsl34IQUjzwDUf\ncWRTYIBzDTWYfL+hZNktx/VZ5rn1sLgnXRRim8TYc7khsXKKnLC3LraVRyoVSw1Z\nF+WnOr9UNIGq4pE3W9vDIZSTtbz8laY1Ym1RVRNmTy2dbfQb4tQqAjfGUz5HoyXr\nLRzahxnQhn26LF7FJbEU+Spz/WD1y/tmknyKmt1+TDsgH+dVwiieemw9KUU55OiL\n2fDO1QIcyN73b8aDi7sSSqmX5wKCfkZwoQPylr1nefae19+DfDbwWLs3RyBMNtqc\ncsJlTfVSbrOY385akmys8HRVvYgYlt80mo2kRkTbSQKBgQDn+g5l5GslpoUlKKOu\nqrtEqN8Euob1VddF3vjdv7IQGn8l/WMa0VeWKrPVkT9dDbuXKxn2obzdRZ8dLo5s\nrVsfu3fO+RcEi2x9ibTMuUFuNFvu0Fx7MPmFwXYQhe/UXWAU9p5vLfhvzr4zV2Yr\nSn0fsUTmHccqTM2DtJXUjKgOpwKBgQDdLgHFuyyXU3PpcL5o5gS31YIwRVUaHE7q\nwsmVOPPF+S0JH1CA+RjulE4FrMUb+MMyEr6OfU3tksjTzsJJg9rxADZ2PjvRG+Rb\nQVZ9Ekynqu93U7QbWU+txK6I9Uw2xl5JIHQZyiNGChMnAK+2y9+0k47jYiwUBHkr\ntAElKYKGaQKBgQCoFTllftJcH4IN4JppLvAt2aZuiLDNBvvKdrsgAYwFuw0x+51Z\niyHJfvt63Zlp1U99Qw+28o4kThPyUw+Dk0CZh35SC44wDs33UFYemITeUzXhnjvE\nT0IdNoZThpi16Up7Jg66RDbk3IpYLRWsfHaZBhsEGcN71NZ8fdaqgZgrtQKBgEVd\nLU8ekTBwCpT2N7DcGjSw7+BFjhffMtyq898pekHnEDmhKXUZKbmApytecH6COG0W\ntc4n++16sCLE3+eAQ4R7RZk1kTrWHXqE9iPJPmMC15C7nmtJShS77uSi0SEIev9B\n/0crPn6zoCgGLhUMwP53nEMxQZORh5KuJAHMj9OpAoGAaTXvLuwGs3yVOLtz6qd1\ns9tGqadhYciNRDsS4/TclOW3FwnKDRv0ChJR2JXotch2fEMUV+Db3bKZafENX/Ic\nbr/L64zFCBuZuWVG4miZeTZ9jmYMdVXxBmOR6eBrPs/u0IjIdEC0At1CTj8xcwBc\ngzGqpVqseFAzde7pS9cRtdw=\n-----END PRIVATE KEY-----\n",
-        "client_email": "bloom-560m@bloom-560m-oxcs.iam.gserviceaccount.com",
-        "token_uri": "https://oauth2.googleapis.com/token",
-    }
-)
+credentials = service_account.Credentials.from_service_account_file(
+    "./bloom-560m-oxcs-0d0eb16ef2b6.json")
 
 # Crie um cliente Dialogflow
 intents_client = dialogflow.IntentsClient(credentials=credentials)
@@ -35,7 +34,7 @@ agent_path = f"projects/{project_id}/agent"
 
 def generate(prompt):
     input_ids = tokenizer(prompt, return_tensors="pt")
-    sample = model.generate(**input_ids, max_length=256, temperature=0.1,)
+    sample = model.generate(**input_ids, max_length=256, temperature=0.1)
 
     return tokenizer.decode(sample[0], truncate_before_pattern=[r"\n\n^#", "^”'", "\n\n\n"])
 
@@ -76,7 +75,7 @@ def webhook():
     print("Query", req_data["queryResult"]["queryText"])
     # print("Query", req_data["queryResult"])
 
-    create_intent(req_data["queryResult"]["queryText"])
+    executor.submit(create_intent, req_data["queryResult"]["queryText"])
 
     res = {
         "fulfillmentText": "I'm learning yet",
